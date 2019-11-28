f3ad328f114d9b65c9eb5c46477eb9f180a96dba
Student <student@Makerss-MacBook-Pro-8.local>
2019-11-28 11:41:39 +0000

    can return 1, multiple or no notes as HTML string

--- spec/note-view-tests.js
+++ spec/note-view-tests.js
@@ -7,13 +7,20 @@ function testReturnOneHTML() {
 };
 testReturnOneHTML();
 
-// function testReturnMultipleHTML() {
-//   var noteList = new NoteList;
-//   var note = new Note("Hello");
-//   var note1 = new Note("Goodbye");
-//   noteList.createAndStore(note);
-//   noteList.createAndStore(note1);
-//   var noteListView = new NoteListView(noteList)
-//   assert.isTrue(noteListView.returnHTML() === "<ul><li><div>Hello</div></li><li><div>Goodbye</div></li></ul>");
-// };
-// testReturnMultipleHTML();
+function testReturnMultipleHTML() {
+  var noteList = new NoteList;
+  var note = new Note("Hello");
+  var note1 = new Note("Goodbye");
+  noteList.createAndStore(note);
+  noteList.createAndStore(note1);
+  var noteListView = new NoteListView(noteList)
+  assert.isTrue(noteListView.returnHTML() === "<ul><li><div>Hello</div></li><li><div>Goodbye</div></li></ul>");
+};
+testReturnMultipleHTML();
+
+function testReturnNoHTML() {
+  var noteList = new NoteList;
+  var noteListView = new NoteListView(noteList)
+  assert.isTrue(noteListView.returnHTML() === "<ul></ul>");
+};
+testReturnNoHTML();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,11 +1,20 @@
 (function(exports) {
   function NoteListView(noteList) {
     this.list = noteList
-  };
+  }
 
   NoteListView.prototype.returnHTML = function() {
-    return "<ul><li><div>" + this.list.array[0] + "</div></li></ul>"
-  };
+    var aStringOfHTML = []
+
+    for (var i = 0; i < this.list.array.length; i++) {
+      aStringOfHTML.push("<li><div>" + this.list.array[i] + "</div></li>")
+    };
+
+    aStringOfHTML.unshift("<ul>")
+    aStringOfHTML.push("</ul>")
+
+    return aStringOfHTML.join("");
+  }
 
   exports.NoteListView = NoteListView;
 })(this);
